5f502ddc9835a809c646be665e9b4f00d3f68ab7
Francyleo Matos <programadorleomatos@gmail.com>
2022-02-10 21:45:15 -0300

    feat: use banner in home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-    <section></section>
+  <div class="container text-white">
+    <Banner
+      title="Welcome."
+      description="Millions of movies, TV shows and people to discover. Explore now."
+      hasSearch
+    />
   </div>
 </template>
 
 <script>
-export default {};
+import layout from '@/mixins/layout';
+
+export default {
+  mixins: [layout],
+};
 </script>
 
 <style lang="scss" scoped></style>
