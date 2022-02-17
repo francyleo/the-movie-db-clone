25a833271f5171bed6455c9943c3cbc8a6a26355
Francyleo Matos <programadorleomatos@gmail.com>
2022-02-17 04:30:13 -0300

    👽️ get movies

--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,27 +1,75 @@
 import api from '@services/api';
 
 const initial_state = {
-  popular: [],
+  movies: {
+    adventure: [],
+    action: [],
+    horror: [],
+    comedy: [],
+    family: [],
+    fantasy: [],
+    war: [],
+  },
 };
 
 export default {
   namespaced: true,
   state: () => initial_state,
   mutations: {
-    SET_POPULAR(state, payload) {
-      state.popular = payload.map(movie => ({
+    SET_MOVIE(state, { gender, payload }) {
+      state.movies[gender] = payload.map(movie => ({
         ...movie,
-        poster_path: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        poster_path: `https://image.tmdb.org/t/p/w185${movie.poster_path}`,
       }));
     },
   },
   actions: {
     async getPopular({ commit }) {
-      const { data } = await api.get('/movie/popular', { group: 'streaming' });
-      commit('SET_POPULAR', data.results);
+      // mock genders
+      const genders = [
+        {
+          id: 12,
+          name: 'adventure',
+        },
+        {
+          id: 28,
+          name: 'action',
+        },
+        {
+          id: 27,
+          name: 'horror',
+        },
+        {
+          id: 35,
+          name: 'comedy',
+        },
+        {
+          id: 10751,
+          name: 'family',
+        },
+        {
+          id: 14,
+          name: 'fantasy',
+        },
+        {
+          id: 10752,
+          name: 'war',
+        },
+      ];
+
+      for (let gender of genders) {
+        const { data } = await api.get('/movie/popular', {
+          with_genres: gender.id,
+        });
+
+        commit('SET_MOVIE', {
+          gender: gender.name,
+          payload: data.results,
+        });
+      }
     },
   },
   getters: {
-    popular: state => state.popular,
+    movies: state => state.movies,
   },
 };
